97aa29da638f3ca94d7bbaff33cd0899eea47085
slav-mirol <slavmirol@gmail.com>
2023-05-16 21:49:11 +0400

    свертан экран cart

--- src/components/cart-product/cart-product.jsx
+++ src/components/cart-product/cart-product.jsx
@@ -1,20 +1,54 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import "./cart-product.css";
 
-const CartProduct = (product, quantity) => {
+const CartProduct = ({
+    product,
+    quantity,
+    setTotal,
+    total
+}) => {
+    const [count,setCount] = useState(0);
+    useEffect(() => {
+        setCount(quantity);
+      }, []);
     return (
         <div className="cart-product">
-            <img src={product.image}/>
-            <p>{product.name}</p>
-            <p>{product.price}</p>
-            <button></button>
-            <p>{quantity}</p>
-            <button>
+            <div style={{display:"flex", flexDirection:"row"}}>
+                <div className="cart-img-container">
+                    <img className="cart-img" src={product.image}/>
+                </div>
+                <p className="cart-text"  style={{width:67}}>{product.name}</p>
+            </div>
+            <div className="cart-info">
                 
-            </button>
-            <p>{product.price * quantity}</p>
-            <button>
-
-            </button>
+                <p className="cart-text" style={{marginLeft:50}} >${product.price}</p>
+                <div style={{display:"flex", flexDirection:"row", alignItems:"center"}}>
+                    <button className="btn-table" style={{marginLeft:165}} onClick={
+                        () => {
+                            if(count>1) {
+                                setCount(count - 1);
+                                setTotal(total - product.price);
+                            }
+                        }
+                    }>
+                        <p className="cart-text">-</p>
+                    </button>
+                    <p className="cart-text" style={{marginLeft:9,marginRight:9,}}>{count}</p>
+                    <button className="btn-table" onClick={
+                        () => {
+                            setCount(count + 1);
+                            setTotal(total + product.price);
+                        }
+                    }>
+                        <p className="cart-text">+</p>
+                    </button>
+                </div>
+                <p className="cart-text" style={{marginLeft:168}}>${product.price * count}</p>
+                <button className="btn-table" style={{marginTop:48, marginLeft:50}}>
+                    <p className="cart-text">X</p>
+                </button>
+            </div>
         </div>
     );
 }
--- src/screens/cart/cart.jsx
+++ src/screens/cart/cart.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import './cart.css';
 import Navigation from '../../components/navigation/navigation';
 import Logo from '../../components/logo1';
+import Products from '../../components/array-products';
+import CartProduct from '../../components/cart-product/cart-product';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Cart = (props) => {
+    const [total, setTotal] = useState(Products[0].price*2+Products[1].price*3);
   return (
     <div className="scr-cart">
         <Logo/>
@@ -13,14 +18,33 @@ const Cart = (props) => {
         <div style={{display: "flex", justifyContent: "center"}}>
             <div className='cart-table'>
                 <div className='head-table'>
-                    <p className='title-table'> ITEM </p>
+                    <p className='title-table' style={{marginLeft:25}}> ITEM </p>
                     <p className='title-table'> PRICE </p>
                     <p className='title-table'> QUANTITY </p>
-                    <p className='title-table'> TOTAL </p>
+                    <p className='title-table' style={{marginRight:40}}> TOTAL </p>
                 </div>
-                <div className='line-table'></div>
+                <div className='line-table'/>
+                <CartProduct product={Products[0]} quantity={2} setTotal={setTotal} total={total}/>
+                <CartProduct product={Products[1]} quantity={3} setTotal={setTotal} total={total}/>
+                <div className='line-table'/>
+                <div className='total-info'>
+                    <div className='total-info-text'>
+                        <p className='total-text'>SUBTOTAL:</p>
+                        <p className='total-text' style={{marginLeft:70}}> ${total}</p>
+                    </div>
+                    <div className='line-table' style={{width: 177, marginTop:11,marginBottom:0}}/>
+                    <div style={{display:"flex",justifyContent:"flex-end"}}>
+                        <button className='btn-add-cart' style={{marginTop:34, height:26, width: 61}}>
+                            <p className='btn-text-add-cart'>
+                                ORDER
+                            </p>
+                        </button>
+                    </div>
+                </div>
+                
+                
             </div>
-            <CartProduct />
+            
         </div>
     </div>
   );
